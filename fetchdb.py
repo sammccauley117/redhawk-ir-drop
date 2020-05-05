13987dec62efdf4d33d41ca46ccf2618d3728e3a
Sam McCauley <sammccauley117@gmail.com>
2020-05-05 15:44:11 -0400

    added more detailed --help

--- fetchdb.py
+++ fetchdb.py
@@ -3,10 +3,29 @@
 import argparse
 import sqlite3
 import os, sys
+from argparse import RawTextHelpFormatter
+
 
 # Set up and parse command line arguments
-parser = argparse.ArgumentParser(description='''Runs an IR drop analysis comparing
-    the values of .cdev, .spiprof, and .pgarc files''')
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, description='\n'.join([
+    'Allows for simple fetch commands straight from the command line to allow users to easily see the data being returned.',
+    '',
+    'Tables and Columns:',
+    '\t- cdev [cell, temperature, state, vector, active_input, active_output, vpwr, vgnd, pin, esc, esr, leak, filename]',
+    '\t- spiprof [cell, vpwr, c1, r, c2, slew1, slew2, state, vector, active_input, active_output, pin, peak, area, width, filename]',
+    '\t- pgarc [cell, pin]',
+    '\t- lib [cell, area, filename]',
+    '',
+    'Examples:',
+    '\t1) To grab all data pertaining to the cell dffnrq_1x from cdev:',
+    '\t$ python3 fetchdb.py "SELECT * FROM cdev WHERE cell=\'dffnrq_1x\'"',
+    '',
+    '\t2) To grab all unique state variations from spiprof:',
+    '\t$ python3 fetchdb.py "SELECT DISTINCT state FROM spiprof ORDER BY state"',
+    '',
+    '\t3) To grab the cells with the highest leakage current:',
+    '\t$ python3 fetchdb.py "SELECT DISTINCT cell, leak FROM cdev ORDER BY leak DESC LIMIT 25"'
+]))
 parser.add_argument('query', help='''SQL query to execute''')
 parser.add_argument('-d', '--database', type=str, default='./redhawk.db', help='File path for the database')
 args = parser.parse_args()
